f1d605cc6f5add8cc1abc3afa1f4756231be1e88
SteveMutu <mutusteve0@gmail.com>
2020-02-16 10:27:51 +0300

    add changes

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function akanName
  Var daysOfWeek=["Sunday", "Monday", "Tuesday","Wednesday", "Thursday", "Friday", "Saturday"]
  
  var yy=parseInt(document.getElementById("year").value);
- var cc=document.getElementById("year").value,slice(0,2);
+ var cc=document.getElementById("year").value.slice(0,2);
  var mm=parseInt(document.getElementById("month").value);
  var dd=parseInt(document.getElementById("dayOfWeek").value);
 
@@ -26,6 +26,6 @@ else if (year===""&&year<1&&year>2024){
     alert("year must be 4 characters")
 }
 else {
-var daysOfWeek =  ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7 );
+var daysOfWeek =  ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
     return daysOfWeek;
 }
