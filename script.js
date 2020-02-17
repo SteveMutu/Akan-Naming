3e07f76f32686fcdeb7e1893d824a2d93500ac88
SteveMutu <mutusteve0@gmail.com>
2020-02-17 10:28:50 +0300

    add changes

--- script.js
+++ script.js
@@ -1,45 +1,114 @@
- //Event funtion on button
-function akanName(){
-var maleNames =["Kwasi", "Kwando", "Kwambena", "Kwaku", "yaw", "Kofi", "Kwame"]
-var femaleNames =["Akosua", "Adowa", "Abenaa", "Akua", "Yaa", "Ama"]
-var daysOfWeek=["Sunday", "Monday", "Tuesday","Wednesday", "Thursday", "Friday", "Saturday"]
- 
- var yy=parseInt(document.getElementById("year").value);
- var cc=document.getElementById("year").value.slice(0,2);
- var mm=parseInt(document.getElementById("month").value);
- var dd=parseInt(document.getElementById("dayOfWeek").value);
+var weekday = function () {
+    var d = new Date();
+    var day1 = document.getElementById("bday").value;
+    var day2 = day1.split("-");
+    var day=day2[2];
+    var month = day2[1];
+    var year = day2[0];
+    d.setDate(parseInt(day));
+    d.setMonth(parseInt(month) - 1);
+    d.setFullYear(parseInt(year));
+     if ((month == 4 || month == 6 || month == 9 || month == 2 || month == 11) && parseInt(day) > 30) {
+       alert("invalid date")
+         return false;
+      } else if (month == 2 && parseInt(year) % 100 === 0 && parseInt(year) % 400 !== 0 && parseInt(day) > 28) {
+        alert("invalid date")
+        return false;
+      }
+      else if (month == 2 && parseInt(year) % 4 !== 0 && parseInt(day) > 28) {
+        alert("invalid date")
+         return false;
+      }
+      else if (month == 2 && parseInt(day) > 29) {
+        alert("invalid date")
+         return false;
+      } else {
+        return d.getDay();
+      }
+}
+// function centuryFromYear(year) {
+//     var x = Math.floor(year/100) + 1;
+//     return x;
+// }
+function akanFemale() {
+    var femaleArray = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]; //sunday-saturday# sunday has index 0
+    if (weekday() === 0) {
+        return "Your Akan name is " + femaleArray[0];
+    }
+    else if (weekday() === 1) {
+        return "Your Akan name is " + femaleArray[1];
+    } else if (weekday() === 2) {
+        return "Your Akan name is " + femaleArray[2];
+    }
+    else if (weekday() === 3) {
+        return "Your Akan name is " + femaleArray[3];
+    }
+    else if (weekday() === 4) {
+        return "Your Akan name is " + femaleArray[4];
+    }
+    else if (weekday() === 5) {
+        return "Your Akan name is " + femaleArray[5];
+    }
+    else {
+        return "Your Akan name is " + femaleArray[6];
+    }
+}
+function akanMale() {
+    var maleArray = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];//sunday has index 0
+    if (weekday() === 0) {
+        return "Your Akan name is " + maleArray[0];
+    }
+    else if (weekday() === 1) {
+        return "Your Akan name is " + maleArray[1];
+    } else if (weekday() === 2) {
+        return "Your Akan name is " + maleArray[2];
+    }
+    else if (weekday() === 3) {
+        return "Your Akan name is " + maleArray[3];
+    }
+    else if (weekday() === 4) {
+        return "Your Akan name is " + maleArray[4];
+    }
+    else if (weekday() === 5) {
+        return "Your Akan name is " + maleArray[5];
+    }
+    else {
+        return "Your Akan name is " + maleArray[6];
+    }
+}
+//output function
+function output() {
+    var day1 = document.getElementById("bday").value;
+    var day2 =day1.split("-");
+    var day= parseInt(day2[2]);
+    var month =parseInt(day2[1]);
+    var year = parseInt( day2[0]);
+    if ((document.getElementById("rad1").checked) && (isNaN(day) || day < 1 || day > 31)) {
+        alert("Please correctly fill day field");
+        return false;
+    }
+    else if (isNaN(month) || month < 1 || month > 12 ||month===null ) {
+        alert("Please correctly fill month field");
+        return false;
+    }
+    else if (isNaN(year) || year < 1582 || year > 2020) {
+        alert("Invalid year");
+        // document.getElementById("akanForm").reset();
+    }
+    else if (document.getElementById("rad1").checked) {
+        document.getElementById("display").innerHTML = akanFemale();
+        document.getElementById("akanForm").reset();
+    }
+    else if (document.getElementById("rad2").checked) {
+        document.getElementById("display").innerHTML = akanMale();
+        document.getElementById("akanForm").reset();
+    } else {
+        alert("all fields must be filled")
+        return false;//prevent refresh
+    }
+}
 
- //century and year
- cc=parseInt+1(century);
-year=document.getElementById("year").value.slice(2,4);
-year=parseInt(year);
 
 
- // birth day details validation validation 
- if(date===""&&date<1&&date>31){
-    alert("date must be between 1 and 31")
-}
-else if(month===""&&month<1&&month>12){
-    alert("month must be between 1 and 12")
-}
-else if (year===""&&year<1&&year>2024){
-    alert("year must be 4 characters")
-}
-else {
-var daysOfWeek =  ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
-    return daysOfWeek;
-}
-//on gender
-function getGender
-var selectGender =document.getElementById("select gender").value;
-if (selectGender==1){
-    gender-"male"
-}
-else if(selectGender==2){
-    gender ="female"
-}
-else{
-    alert("select Gender");
-}
-return gender;
-}
+
+
